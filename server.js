81c77c440804e72497a9cc96d074044238396a42
Glitch (aeolian-dog) <none>
2019-12-25 11:52:06 +0000

    🚂🍞 Checkpoint ./server.js:9636892/389 ./package.json:9636892/947

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
-
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 // require and use "multer"...
 
 var app = express();
@@ -13,6 +14,11 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
+
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
+  console.log(req.file);
+  res.json({"name": req.file.originalname, "type": req.file.mimetype, "size": req.file.size});
+});
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
